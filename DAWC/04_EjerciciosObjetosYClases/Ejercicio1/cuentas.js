81fc0d49d66d69ed5bc1076c8f3354afaec31eac
jesusglez96 <jezu96@gmail.com>
2021-10-14 21:56:33 +0200

    commit on master

---
+++ DAWC/04_EjerciciosObjetosYClases/Ejercicio1/cuentas.js
@@ -0,0 +1,37 @@
+let cuenta1 = {
+    cuentaBancaria:1,
+    titular:"Jesus",
+    saldo:2000,
+    ingresar:function(dinero){
+        this.saldo+=dinero;
+    },
+    retirar:function(dinero){
+        this.saldo-=dinero;
+    }
+}
+let cuenta2 = {
+    cuentaBancaria:2,
+    titular:"capiYTHD360XD",
+    saldo:0,
+    ingresar:function(dinero){
+        this.saldo+=dinero;
+    },
+    retirar:function(dinero){
+        this.saldo-=dinero;
+    }
+}
+let cuenta3 = {
+    cuentaBancaria:3,
+    titular:"marialuisa",
+    saldo:1000000,
+    ingresar:function(dinero){
+        this.saldo+=dinero;
+    },
+    retirar:function(dinero){
+        this.saldo-=dinero;
+    }
+}
+
+let arCuentas= [];
+arCuentas.push(cuenta1, cuenta2, cuenta3);
+console.log(arCuentas);
